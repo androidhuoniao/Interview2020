0896b714b59c2e7655e70ca893df12319234eb1b
grass <grassswwang@tencent.com>
2020-09-20 17:07:36 +0800

    实现快排 快排的难点在于 1. 三分取中，中间值存放在low位置 2. while循环的判断

--- src/com/grass/interview/suanfa/sort/QuickSortMain.java
+++ src/com/grass/interview/suanfa/sort/QuickSortMain.java
@@ -7,79 +7,99 @@ package com.grass.interview.suanfa.sort;
  */
 public class QuickSortMain {
     public static void main(String[] args) {
-//        int[] array = new int[]{5, 1, 9, 3, 7, 6, 8, 5, 2};
-        int[] array = new int[]{9, 1, 5, 8, 3, 7, 4, 6, 2};
-//        int pivot = getPivot(array);
-//        int pivot2 = getPivot(array, 0, array.length - 1);
-//        System.out.println("pivot: " + pivot + " pivot2: " + pivot2);
-//        qSort(array, 0, array.length - 1);
+        test1();
+        test2();
+        test3();
+        test4();
+    }
+
+    private static void test1() {
+        int[] array1 = new int[]{5, 1, 9, 3, 7, 6, 8, 5, 2};
+        sort(array1, 0, array1.length - 1);
+        System.out.println(getLogStr(array1));
+    }
+
+    private static void test2() {
+        int[] array2 = new int[]{9, 1, 5, 8, 3, 7, 4, 6, 2};
+        sort(array2, 0, array2.length - 1);
+        System.out.println(getLogStr(array2));
+    }
+
+    private static void test3() {
+        int[] array3 = new int[]{50, 10, 90, 30, 70, 40, 80, 60, 20};
+        sort(array3, 0, array3.length - 1);
+        System.out.println(getLogStr(array3));
+    }
+
+    private static void test4() {
+        int[] array = new int[]{4, 5, 7, 8, 1, 2, 3, 6};
         sort(array, 0, array.length - 1);
         System.out.println(getLogStr(array));
-
     }
 
     private static void sort(int[] array, int low, int high) {
-        int pivot = qSort(array, low, high);
-        if (low < pivot) {
+        int pivot = 0;
+        if (low < high) {
+            pivot = qSort(array, low, high);
             sort(array, low, pivot - 1);
-        }
-        if (pivot < high) {
             sort(array, pivot + 1, high);
         }
     }
 
+    /**
+     * 第一版，整个有问题啊
+     *
+     * @param array
+     * @param low
+     * @param high
+     * @return
+     */
     private static int qSort(int[] array, int low, int high) {
         int pivot = getPivot(array, low, high);
+        System.out.println("qSort: pivot: " + pivot);
         while (low < high) {
-            while (array[pivot] <= array[high]) {
+            while (array[high] >= pivot && low < high) {
                 high--;
             }
-            swap(array, pivot, high);
-            while (array[low] <= array[pivot]) {
+            swap(array, low, high);
+            while (array[low] <= pivot && low < high) {
                 low++;
             }
-            swap(array, pivot, low);
-        }
-        return pivot;
-    }
-
-    private static int getPivot(int[] array) {
-        int pivot = 0;
-        if (array == null || array.length <= 2) {
-            return pivot;
-        }
-        int low = 0;
-        int high = array.length - 1;
-        int mid = (high + low) / 2;
-        if (array[low] > array[high]) {
             swap(array, low, high);
         }
-        if (array[low] > array[mid]) {
-            swap(array, low, mid);
-        }
-        if (array[mid] > array[high]) {
-            swap(array, mid, high);
-        }
-        pivot = array[mid];
-        return pivot;
+        return low;
     }
 
+    /**
+     * 三数取中法，然后将中间值存储在low的位置，为什么要存储在low的位置，我也不懂
+     * https://www.yuque.com/wangshengwei-evzp3/te8s4z/ge0pt3
+     *
+     * @param array
+     * @param low
+     * @param high
+     * @return
+     */
     private static int getPivot(int[] array, int low, int high) {
         int pivot = 0;
         if (array == null || array.length <= 2) {
+            pivot = low;
             return pivot;
         }
         int mid = (high + low) / 2;
+        String oldList = "low: " + array[low] + " mid: " + array[mid] + " high: " + array[high];
         if (array[low] > array[high]) {
             swap(array, low, high);
-        }
-        if (array[low] > array[mid]) {
-            swap(array, low, mid);
         }
         if (array[mid] > array[high]) {
             swap(array, mid, high);
         }
-        pivot = array[mid];
+        if (array[mid] > array[low]) {
+            swap(array, low, mid);
+        }
+//        String newList = "low: " + array[low] + " mid: " + array[mid] + " high: " + array[high];
+//        System.out.println("oldList: " + oldList + "\n" + "newList: " + newList);
+//        System.out.println(getLogStr(array));
+        pivot = array[low];
         return pivot;
     }
 
